369b1ae1a433004919326614fd48837d392d12e2
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2021-12-02 09:16:22 +0100

    Superbucle de la muerte

--- HolaMundo/src/HolaMundo/HolaMundo.java
+++ HolaMundo/src/HolaMundo/HolaMundo.java
@@ -5,6 +5,18 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hello World all the World");
+
+		int[] array = new int[3];
+		array[0] = 123;
+		array[1] = 456;
+		array[2] = 789;
+
+		System.out.println(array[0]);
+		System.out.println(array[1]);
+		System.out.println(array[2]);
+
+
+
 	}
 
 }
