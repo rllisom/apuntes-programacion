34b07f30c670047d38ea052a6570c02e4b1cf794
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2021-12-02 09:25:49 +0100

    Bucle en condisiones

--- HolaMundo/src/HolaMundo/HolaMundo.java
+++ HolaMundo/src/HolaMundo/HolaMundo.java
@@ -3,8 +3,15 @@ package HolaMundo;
 public class HolaMundo {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
 		System.out.println("Hello World all the World");
+
+		int[] arr = new int[] {123, 456, 789};
+
+		for(int a: arr) {
+			System.out.println(a);
+		}
+
+
 	}
 
 }
