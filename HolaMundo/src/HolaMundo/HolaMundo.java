c2dd844558c019961a895bab3b7a71fc016e314e
Luis Miguel López <luismi.lopez@triana.salesianos.edu>
2025-01-16 10:40:28 +0100

    Mejoras en el código al imprimir un array

--- HolaMundo/src/HolaMundo/HolaMundo.java
+++ HolaMundo/src/HolaMundo/HolaMundo.java
@@ -5,18 +5,23 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hello World all the World");
 
-		int[] array = new int[3];
+		int[] array = new int[4];
 		array[0] = 123;
 		array[1] = 456;
 		array[2] = 789;
+		array[3] = 999;
 
 		System.out.println(array[0]);
 		System.out.println(array[1]);
 		System.out.println(array[2]);
+		System.out.println(array[3]);
+
+		for (int x : array) {
+			System.out.println(x);
+		}
 
 
-
-
+		System.out.println("\n\n\n");
 		int[] arr = new int[] {123, 456, 789};
 
 		for(int a: arr) {
